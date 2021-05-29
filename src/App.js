0cdf2e75f715569d58910d687d7c34e66c81386c
shifatreza <shifatreza44@gmail.com>
2021-05-29 20:42:45 +0530

    api has been updated and features added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-import { getPosts } from './Redux/actions/posts';
-
+import { getPosts } from './actions/posts';
 import useStyles from './styles';
+import memories from './images/memories.png';
 
-function App() {
-  const classes = useStyles();
+const App = () => {
+  const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   useEffect(() => {
     dispatch(getPosts());
-  }, [dispatch]);
+  }, [currentId, dispatch]);
 
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Evocation</Typography>
-        {/* <img className={classes.image} src={memories} alt="icon" height="60" /> */}
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
           <Grid container justify="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
-              <Posts />
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form />
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
       </Grow>
     </Container>
   );
-}
+};
 
 export default App;
