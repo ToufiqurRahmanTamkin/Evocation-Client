69eb5767d5bb82aae05d8bd8c52d4bddef66f054
shifatreza <shifatreza44@gmail.com>
2021-05-29 11:23:03 +0530

    commit from shifat

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Testing from './components/Testing';
 
 function App() {
   return (
     <div className="App">
       <h1>This is shifat</h1>
+      <Testing></Testing>
     </div>
   );
 }
---
+++ src/components/Testing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Testing = () => {
+    return (
+        <div>
+            <h1>This is testing...</h1>
+        </div>
+    );
+};
+
+export default Testing;
